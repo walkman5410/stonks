1f36864d68db178fc042b2a1da09e75a5a3fe897
Tim <walkman5410@yahoo.com>
2021-07-10 12:37:19 -0400

    stonksisprojectname

--- stonks/stonks/job8pillarPull.py
+++ stonks/stonks/job8pillarPull.py
@@ -1,5 +1,5 @@
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pidjango.settings") 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stonks.settings") 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 from django.conf import settings
