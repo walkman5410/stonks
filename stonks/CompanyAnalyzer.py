08128d412581f96eaf4e4b4982b209a2f8e464ea
Tim <walkman5410@yahoo.com>
2021-07-10 12:51:13 -0400

    company analyzer update

--- stonks/CompanyAnalyzer.py
+++ stonks/CompanyAnalyzer.py
@@ -1,73 +1,85 @@
-import yfinance as yf
 import pandas as pd
-import yahoo_fin.stock_info as si
-from pprint import pprint
 import json
+import requests
+from lxml import etree 
 
+def getEightPillars(tickerSymbol):   
+    site = 'https://finance.yahoo.com/quote/'+tickerSymbol+'/balance-sheet?p='+tickerSymbol
+    headers = {'User-agent': 'Mozilla/5.0'}
+    html = requests.get(url=site, headers=headers).text
+    json_str = html.split('root.App.main =')[1].split('(this)')[0].split(';\n}')[0].strip()
+    json_data = json.loads(json_str)  
 
-def getEightPillars(tickerSymbol):
-    quoteTbl = si.get_quote_table(tickerSymbol)
-    income_statement = si.get_income_statement(tickerSymbol)
-    balance_sheet = si.get_balance_sheet(tickerSymbol)
-    cash_flow_statement = si.get_cash_flow(tickerSymbol)
+    site2 = 'https://www.sharesoutstandinghistory.com/?symbol='+tickerSymbol
+    html2 = requests.get(url=site2, headers=headers).text
+    tree = etree.HTML(html2)
 
-    yf_data = yf.Ticker(tickerSymbol)
+    company_name = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['quoteType']['longName']
 
-    currPrice = quoteTbl["Quote Price"]
+    currPrice = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['price']['regularMarketPrice']['raw']
 
-    sharesOutstanding = yf_data.info['sharesOutstanding'] 
-    market_cap = yf_data.info['marketCap'] 
-
-    #EPS missing                                                                
-    #                                                                           Formula: (Net Income - Preferred Dividends) / Shares Outstanding
-    #Using Trailing (past performance) EPS
-    Eps = quoteTbl["EPS (TTM)"]
+    sharesOutstanding = json_data['context']['dispatcher']['stores']['StreamDataStore']['quoteData'][tickerSymbol]['sharesOutstanding']['raw']
+    market_cap = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['summaryDetail']['marketCap']['raw']
 
     #PE Ratio missing                                                           
     #                                                                           Formula: Price / EPS
     #Using Trailing (past performance) PE
-    Pe = quoteTbl["PE Ratio (TTM)"] 
-    Pe = Pe if pd.notnull(Pe) else 0
+    Pe = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['summaryDetail']['trailingPE']['raw'] if 'trailingPE' in json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['summaryDetail']  else None
 
-    #determine what a good profit margin is
-    profit_margin = yf_data.info['profitMargins']
-    dividendYield = yf_data.info['dividendYield'] 
     #                                                                           Formula: (Price * Shares Outstanding) * dividend yield
-    dividendsPaid = (currPrice * sharesOutstanding) * dividendYield if dividendYield else None #Ensure the dividends paid is supported by free cash flow
+    dividendsPaid = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][0]['dividendsPaid']['raw'] if 'dividendsPaid' in json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][0] else 0 #Ensure the dividends paid is supported by free cash flow
 
     #4 year income statement comparison
 
     #Revenue
-    latest_revenue = income_statement[income_statement.columns[0]][income_statement.index.get_loc('totalRevenue')] if 'totalRevenue' in income_statement.index else None
-    earliest_revenue = income_statement[income_statement.columns[3]][income_statement.index.get_loc('totalRevenue')] if 'totalRevenue' in income_statement.index else None
+    latest_revenue = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['incomeStatementHistory']['incomeStatementHistory'][0]['totalRevenue']['raw']
+    earliest_revenue = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['incomeStatementHistory']['incomeStatementHistory'][3]['totalRevenue']['raw']
 
     #Net Income
-    latest_net_income = income_statement[income_statement.columns[0]][income_statement.index.get_loc('netIncome')] if 'netIncome' in income_statement.index else None  
-    earliest_net_income = income_statement[income_statement.columns[3]][income_statement.index.get_loc('netIncome')] if 'netIncome' in income_statement.index else None
+    latest_net_income = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['incomeStatementHistory']['incomeStatementHistory'][0]['netIncome']['raw']
+    earliest_net_income = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['incomeStatementHistory']['incomeStatementHistory'][3]['netIncome']['raw']
+    
+    #profit margin calc
+    profit_margin = latest_net_income / latest_revenue # netincome/revenue
 
-    #Shares Outstanding
-    latest_shares_outstanding = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('commonStock')] if 'commonStock' in balance_sheet.index else None  
-    earliest_shares_outstanding = balance_sheet[balance_sheet.columns[3]][balance_sheet.index.get_loc('commonStock')] if 'commonStock' in balance_sheet.index else None   
+    #Using Trailing (past performance) EPS
+    Eps = latest_net_income / sharesOutstanding #Formula: (Net Income - Preferred Dividends) / Shares Outstanding
+
+    #Shares Outstanding    
+    tbl = {}
+    for i in tree.cssselect('td.tstyle'):
+        if i.text == None:
+            key = i[0].text
+        else:
+            tbl[key] = i.text
+    df = pd.DataFrame.from_dict(tbl, orient='index',columns=['sharesOutstanding']) 
+    df1 = df['sharesOutstanding'].str.strip('$').str.extract(r'(\d+\.\d+)([BMK]+)')
+    df['sharesOutstanding'] = df1[0].astype(float) * df1[1].map({'B': 1000000000, 'M':1000000, 'K':1000})                
+    latest_shares_outstanding = int(df.iloc[[-1]]['sharesOutstanding']) 
+    try:
+        earliest_shares_outstanding = int(df.iloc[[-21]]['sharesOutstanding'])    
+    except IndexError:
+        earliest_shares_outstanding = 0
 
     #Current Assets over Current Liabilities
-    current_assets = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('totalCurrentAssets')] if 'totalCurrentAssets' in balance_sheet.index else None 
-    current_liabilities = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('totalCurrentLiabilities')] if 'totalCurrentAssets' in balance_sheet.index else None 
+    current_assets = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['balanceSheetHistory']['balanceSheetStatements'][0]['totalCurrentAssets']['raw']
+    current_liabilities = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['balanceSheetHistory']['balanceSheetStatements'][0]['totalCurrentLiabilities']['raw']
 
     try:
         #Free Cash Flow
         #                                                                                       Formula: Cash from Operations - Net Change in Capital Expenditures
-        latest_cash_from_operations = cash_flow_statement[cash_flow_statement.columns[0]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')]
-        latest_capital_expenditures = cash_flow_statement[cash_flow_statement.columns[0]][cash_flow_statement.index.get_loc('capitalExpenditures')]
+        latest_cash_from_operations = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][0]['totalCashFromOperatingActivities']['raw']
+        latest_capital_expenditures = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][0]['capitalExpenditures']['raw']
         latest_free_cash_flow = latest_cash_from_operations + latest_capital_expenditures
-        earliest_cash_from_operations = cash_flow_statement[cash_flow_statement.columns[3]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')]
-        earliest_capital_expenditures = cash_flow_statement[cash_flow_statement.columns[3]][cash_flow_statement.index.get_loc('capitalExpenditures')]
+        earliest_cash_from_operations = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][3]['totalCashFromOperatingActivities']['raw']
+        earliest_capital_expenditures = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][3]['capitalExpenditures']['raw']
         earliest_free_cash_flow = earliest_cash_from_operations + earliest_capital_expenditures
 
 
         free_cash_flow = 0
         for i in range(0, 4):
-            cash_from_operations = cash_flow_statement[cash_flow_statement.columns[i]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')]
-            capital_expenditures = cash_flow_statement[cash_flow_statement.columns[i]][cash_flow_statement.index.get_loc('capitalExpenditures')]
+            cash_from_operations = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][i]['totalCashFromOperatingActivities']['raw']
+            capital_expenditures = json_data['context']['dispatcher']['stores']['QuoteSummaryStore']['cashflowStatementHistory']['cashflowStatements'][i]['capitalExpenditures']['raw']
             free_cash_flow += (cash_from_operations + capital_expenditures)
         average_cash_flow = free_cash_flow/4
         cash_flow_minus_dividend = average_cash_flow - dividendsPaid if dividendsPaid else average_cash_flow
@@ -81,25 +93,32 @@ def getEightPillars(tickerSymbol):
 
     data = {}
 
+    data['company_name'] = company_name
     data['market_cap'] = market_cap
-    data['Eps'] = int(Eps)
-    data['Pe'] = int(Pe) if pd.notnull(Pe) else int(0)
-    data['is_pe_acceptable'] = bool(Pe < 20) if pd.notnull(Pe) else False
+    data['Eps'] = int(Eps) if pd.notnull(Eps) else int(0)
+    data['Pe'] = int(Pe)
+    data['is_pe_acceptable'] = bool(Pe < 20) if pd.notnull(Pe) and Pe != 0 else False
     data['profit_margin'] = profit_margin
     data['is_profit_margin_acceptable'] = bool(profit_margin > .1)
     data['latest_revenue'] = int(latest_revenue)
+    data['earliest_revenue'] = int(earliest_revenue)
     data['is_revenue_growing'] = bool(latest_revenue > earliest_revenue)
     data['latest_net_income'] = int(latest_net_income)
+    data['earliest_net_income'] = int(earliest_net_income)
     data['is_net_income_growing'] = bool(latest_net_income > earliest_net_income)
-    data['latest_shares_outstanding'] = int(latest_shares_outstanding)
+    data['earliest_shares_outstanding'] = int(earliest_shares_outstanding) if earliest_shares_outstanding else None
+    data['latest_shares_outstanding'] = int(latest_shares_outstanding) if latest_shares_outstanding else None
     data['shares_outstanding'] = int(sharesOutstanding)
-    data['are_shares_outstanding_shrinking'] = bool(latest_shares_outstanding < earliest_shares_outstanding)
+    data['are_shares_outstanding_shrinking'] = bool(latest_shares_outstanding < earliest_shares_outstanding) if earliest_shares_outstanding and latest_shares_outstanding else None
+    data['quick_ratio'] = (current_assets / current_liabilities)
     data['is_quick_ratio_positive'] = bool((current_assets / current_liabilities) > 1)
-    data['is_cash_flow_growing'] = bool(latest_free_cash_flow > earliest_free_cash_flow)
-    data['average_cash_flow'] = int(average_cash_flow)
-    data['is_dividend_yield_affordable'] = bool(cash_flow_minus_dividend > 0)
-    data['cash_flow_value'] = int(cash_flow_value)
-    data['is_market_price_worth'] = bool(market_cap < cash_flow_value)
+    data['latest_free_cash_flow'] = int(latest_free_cash_flow) if latest_free_cash_flow else None
+    data['earliest_free_cash_flow'] = int(earliest_free_cash_flow) if earliest_free_cash_flow else None
+    data['is_cash_flow_growing'] = bool(latest_free_cash_flow > earliest_free_cash_flow) if latest_free_cash_flow else None
+    data['average_cash_flow'] = int(average_cash_flow) if average_cash_flow else None
+    data['is_dividend_yield_affordable'] = bool(cash_flow_minus_dividend > 0) if cash_flow_minus_dividend else None
+    data['cash_flow_value'] = int(cash_flow_value) if cash_flow_value else None
+    data['is_market_price_worth'] = bool(market_cap < cash_flow_value) if cash_flow_value else None
 
     data = json.dumps(data)
 
