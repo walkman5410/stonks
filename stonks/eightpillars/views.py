2ea2bc4959548ff89c439bed0e33249f9922c69b
Tim <walkman5410@yahoo.com>
2021-07-10 11:08:20 -0400

    updatetoviews

--- stonks/eightpillars/views.py
+++ stonks/eightpillars/views.py
@@ -28,6 +28,7 @@ class HomePage(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['tickerform'] = TickerForm()
         return context
+
 def view_all_eight_pillar_winner_data(request):
     data ={}
     try:
@@ -64,7 +65,7 @@ def get_the_pillars(request):
         balance_sheet = si.get_balance_sheet(tickerSymbol)
         cash_flow_statement = si.get_cash_flow(tickerSymbol)
         yf_data = yf.Ticker(tickerSymbol)
-
+        company_name = yf_data.info['longName']
         currPrice = quoteTbl["Quote Price"]
 
         sharesOutstanding = yf_data.info['sharesOutstanding'] 
@@ -90,62 +91,86 @@ def get_the_pillars(request):
         #4 year income statement comparison
 
         #Revenue
-        latest_revenue = income_statement[income_statement.columns[0]][income_statement.index.get_loc('totalRevenue')]
-        earliest_revenue = income_statement[income_statement.columns[3]][income_statement.index.get_loc('totalRevenue')]
+        latest_revenue = income_statement[income_statement.columns[0]][income_statement.index.get_loc('totalRevenue')] if 'totalRevenue' in income_statement.index else None
+        earliest_revenue = income_statement[income_statement.columns[3]][income_statement.index.get_loc('totalRevenue')] if 'totalRevenue' in income_statement.index else None
         is_revenue_growing = latest_revenue > earliest_revenue
 
         #Net Income
-        latest_net_income = income_statement[income_statement.columns[0]][income_statement.index.get_loc('netIncome')]
-        earliest_net_income = income_statement[income_statement.columns[3]][income_statement.index.get_loc('netIncome')]
+        latest_net_income = income_statement[income_statement.columns[0]][income_statement.index.get_loc('netIncomeFromContinuingOps')] if 'netIncomeFromContinuingOps' in income_statement.index else None  
+        earliest_net_income = income_statement[income_statement.columns[3]][income_statement.index.get_loc('netIncomeFromContinuingOps')] if 'netIncomeFromContinuingOps' in income_statement.index else None
         is_net_income_growing = latest_net_income > earliest_net_income
 
         #Shares Outstanding
-        latest_shares_outstanding = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('commonStock')]
-        earliest_shares_outstanding = balance_sheet[balance_sheet.columns[3]][balance_sheet.index.get_loc('commonStock')]
-        is_shares_outstanding_growing = latest_shares_outstanding > earliest_shares_outstanding
-
+        try:
+            headers = {'User-agent': 'Mozilla/5.0'}
+            html = requests.get(url='https://finance.yahoo.com/quote/'+tickerSymbol+'/balance-sheet?p='+tickerSymbol, headers=headers).text
+            json_str = html.split('root.App.main =')[1].split(
+                '(this)')[0].split(';\n}')[0].strip()
+            json_data = json.loads(json_str)        
+            latest_shares_outstanding = json_data['context']['dispatcher']['stores']['QuoteTimeSeriesStore']['timeSeries']['annualShareIssued'][3]['reportedValue']['raw']  
+            earliest_shares_outstanding = json_data['context']['dispatcher']['stores']['QuoteTimeSeriesStore']['timeSeries']['annualShareIssued'][0]['reportedValue']['raw']  
+        except:
+            latest_shares_outstanding = None
+            earliest_shares_outstanding = None
+            
         #Current Assets over Current Liabilities
-        current_assets = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('totalCurrentAssets')]
-        current_liabilities = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('totalCurrentLiabilities')]
+        current_assets = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('totalCurrentAssets')] if 'totalCurrentAssets' in balance_sheet.index else None 
+        current_liabilities = balance_sheet[balance_sheet.columns[0]][balance_sheet.index.get_loc('totalCurrentLiabilities')] if 'totalCurrentAssets' in balance_sheet.index else None 
         is_quick_ratio_positive = (current_assets / current_liabilities) > 1
 
-        #Free Cash Flow
-        #                                                                                       Formula: Cash from Operations - Net Change in Capital Expenditures
-        latest_cash_from_operations = cash_flow_statement[cash_flow_statement.columns[0]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')]
-        latest_capital_expenditures = cash_flow_statement[cash_flow_statement.columns[0]][cash_flow_statement.index.get_loc('capitalExpenditures')]
-        latest_free_cash_flow = latest_cash_from_operations + latest_capital_expenditures
-        earliest_cash_from_operations = cash_flow_statement[cash_flow_statement.columns[3]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')]
-        earliest_capital_expenditures = cash_flow_statement[cash_flow_statement.columns[3]][cash_flow_statement.index.get_loc('capitalExpenditures')]
-        earliest_free_cash_flow = earliest_cash_from_operations + earliest_capital_expenditures
-        is_latest_cash_flow_positive = latest_free_cash_flow > 0
-        free_cash_flow = 0
-        for i in range(0, 4):
-            cash_from_operations = cash_flow_statement[cash_flow_statement.columns[i]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')]
-            capital_expenditures = cash_flow_statement[cash_flow_statement.columns[i]][cash_flow_statement.index.get_loc('capitalExpenditures')]
-            free_cash_flow += (cash_from_operations + capital_expenditures)
+        try:
+            #Free Cash Flow
+            #                                                                                       Formula: Cash from Operations - Net Change in Capital Expenditures
+            latest_cash_from_operations = cash_flow_statement[cash_flow_statement.columns[0]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')] if 'totalCashFromOperatingActivities' in cash_flow_statement.index else 0
+            latest_capital_expenditures = cash_flow_statement[cash_flow_statement.columns[0]][cash_flow_statement.index.get_loc('capitalExpenditures')] if 'capitalExpenditures' in cash_flow_statement.index else 0
+            latest_free_cash_flow = latest_cash_from_operations + latest_capital_expenditures
+            earliest_cash_from_operations = cash_flow_statement[cash_flow_statement.columns[3]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')] if 'totalCashFromOperatingActivities' in cash_flow_statement.index else 0
+            earliest_capital_expenditures = cash_flow_statement[cash_flow_statement.columns[3]][cash_flow_statement.index.get_loc('capitalExpenditures')] if 'capitalExpenditures' in cash_flow_statement.index else 0
+            earliest_free_cash_flow = earliest_cash_from_operations + earliest_capital_expenditures
 
-        average_cash_flow = free_cash_flow/4
-        cash_flow_minus_dividend = average_cash_flow - dividendsPaid if dividendsPaid else average_cash_flow
 
-        cash_flow_value = average_cash_flow * 20
+            free_cash_flow = 0
+            for i in range(0, 4):
+                cash_from_operations = cash_flow_statement[cash_flow_statement.columns[i]][cash_flow_statement.index.get_loc('totalCashFromOperatingActivities')] if 'totalCashFromOperatingActivities' in cash_flow_statement.index else 0
+                capital_expenditures = cash_flow_statement[cash_flow_statement.columns[i]][cash_flow_statement.index.get_loc('capitalExpenditures')] if 'capitalExpenditures' in cash_flow_statement.index else 0
+                free_cash_flow += (cash_from_operations + capital_expenditures)
+            average_cash_flow = free_cash_flow/4
+            cash_flow_minus_dividend = average_cash_flow - dividendsPaid if dividendsPaid else average_cash_flow
+            cash_flow_value = average_cash_flow * 20
+        except:
+            latest_free_cash_flow = None
+            earliest_free_cash_flow = None
+            average_cash_flow = None
+            cash_flow_minus_dividend = None
+            cash_flow_value = None
 
+        data['ticker'] = tickerSymbol
+        data['company_name'] = company_name
         data['market_cap'] = market_cap
-        data['Eps'] = int(Eps)
+        data['Eps'] = int(Eps) if pd.notnull(Eps) else int(0)
         data['Pe'] = int(Pe) if pd.notnull(Pe) else int(0)
-        data['is_pe_acceptable'] = bool(Pe < 20) if pd.notnull(Pe) else False
-        data['profit_margin'] = profit_margin
+        data['is_pe_acceptable'] = bool(Pe < 20) if pd.notnull(Pe) and Pe != 0 else False
+        data['profit_margin'] = '{0:.3%}'.format(profit_margin)
+        data['is_profit_margin_acceptable'] = bool(profit_margin > .1)
         data['latest_revenue'] = int(latest_revenue)
+        data['earliest_revenue'] = int(earliest_revenue)
         data['is_revenue_growing'] = bool(latest_revenue > earliest_revenue)
         data['latest_net_income'] = int(latest_net_income)
+        data['earliest_net_income'] = int(earliest_net_income)
         data['is_net_income_growing'] = bool(latest_net_income > earliest_net_income)
-        data['latest_shares_outstanding'] = int(latest_shares_outstanding)
-        data['are_shares_outstanding_shrinking'] = bool(latest_shares_outstanding < earliest_shares_outstanding)
+        data['earliest_shares_outstanding'] = int(earliest_shares_outstanding) if earliest_shares_outstanding else None
+        data['latest_shares_outstanding'] = int(latest_shares_outstanding) if latest_shares_outstanding else None
+        data['shares_outstanding'] = int(sharesOutstanding)
+        data['are_shares_outstanding_shrinking'] = bool(latest_shares_outstanding < earliest_shares_outstanding) if latest_shares_outstanding and earliest_shares_outstanding else None
+        data['quick_ratio'] = (current_assets / current_liabilities)
         data['is_quick_ratio_positive'] = bool((current_assets / current_liabilities) > 1)
-        data['is_cash_flow_growing'] = bool(latest_free_cash_flow > earliest_free_cash_flow)
-        data['average_cash_flow'] = int(average_cash_flow)
-        data['is_dividend_yield_affordable'] = bool(cash_flow_minus_dividend > 0)
-        data['cash_flow_value'] = int(cash_flow_value)
-        data['is_market_price_worth'] = bool(market_cap < cash_flow_value)
+        data['latest_free_cash_flow'] = int(latest_free_cash_flow) if latest_free_cash_flow else None
+        data['earliest_free_cash_flow'] = int(earliest_free_cash_flow) if earliest_free_cash_flow else None
+        data['is_cash_flow_growing'] = bool(latest_free_cash_flow > earliest_free_cash_flow) if latest_free_cash_flow else None
+        data['average_cash_flow'] = int(average_cash_flow) if average_cash_flow else None
+        data['is_dividend_yield_affordable'] = bool(cash_flow_minus_dividend > 0) if cash_flow_minus_dividend else None
+        data['cash_flow_value'] = int(cash_flow_value) if cash_flow_value else None
+        data['is_market_price_worth'] = bool(market_cap < cash_flow_value) if cash_flow_value else None   
     else:
         data['message'] = 'You didnt give me the Ticker DUMB DUMB'
         status = 500
@@ -153,7 +178,6 @@ def get_the_pillars(request):
 
 def get_the_pillar_table(request):
     data = {}
-    status = 200
     if request.method == 'GET':
         tickerSymbol = request.GET.get('tickerSymbol').upper()
         #balance_sheet = si.get_balance_sheet(tickerSymbol)
@@ -272,6 +296,5 @@ def get_the_pillar_table(request):
         data['cash_flow_value'] = int(cash_flow_value) if cash_flow_value else None
         data['is_market_price_worth'] = bool(market_cap < cash_flow_value) if cash_flow_value else None        
     else:
-        data['message'] = 'You didnt give me the Ticker DUMB DUMB'
-        status = 500
+        data['message'] = 'You did not give me the Ticker DUMB DUMB'
     return render(request, 'eightpillars/functions/8pillartable.html', data)
